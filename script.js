8280426ff7694ad12e6c377c3af01d5af6f16ef5
kambaledimitri <kambaledimitri@gmail.com>
2021-02-04 14:23:14 +0100

    travail jour 1

---
+++ script.js
@@ -0,0 +1,13 @@
+var x=document.getElementById('ban31');
+x.onmouseover=function () {
+	x.innerHTML='VIVEZ VOTRE VIE EN UTILISANT TRELLO!'
+
+}
+var x=document.getElementById('ban31');
+x.onmouseout=function () {
+	x.innerHTML='WELCOME TO TRELLO!'
+
+}
+
+
+
